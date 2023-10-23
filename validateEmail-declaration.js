d977ae15e25a8e081984ce887e5897c9c2aec395
samuel21695 <samuel21695@gmail.com>
2023-10-23 21:20:42 +0900

    const hasAtSign && hasDot using includes() method

--- validateEmail-declaration.js
+++ validateEmail-declaration.js
@@ -12,6 +12,12 @@ function validateEmail(email){
       } else {
         return false;
       }
-    })
+    });
+
+    // 필터링된 배열에서 '@'와 '.' 문자의 존재 여부를 확인
+    // includes() 메서드는 배열 메서드이다.
+    // 원활한 사용을 위해서는 공식문서(mdn)사용이 필수적이다.
+    const hasAtSign = filteredArray.includes('@');
+    const hasDot = filteredArray.includes('.');
   }
 }
