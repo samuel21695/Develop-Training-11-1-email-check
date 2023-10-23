6169b18ece8279af0ab12a6ff99f3a1ea86b63c1
samuel21695 <samuel21695@gmail.com>
2023-10-23 21:17:59 +0900

    const filteredArray

--- validateEmail-declaration.js
+++ validateEmail-declaration.js
@@ -3,6 +3,15 @@ function validateEmail(email){
     // 문자열을 배열로 변환
     const emailArray = Array.from(email);
 
-    
+    // '@' 와 '.' 문자를 필터링
+    const filteredArray = emailArray.filter(char => {
+      if (char === '@') {
+        return true;
+      } else if (char === '.') {
+        return true;
+      } else {
+        return false;
+      }
+    })
   }
 }
